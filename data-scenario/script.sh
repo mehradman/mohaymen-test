576401a9f3b52b096b60802448fbc5e1c5f18a64
mehradman <m.radmanmehr98@gmail.com>
2025-02-22 10:20:58 +0330

    Fix script

--- data-scenario/script.sh
+++ data-scenario/script.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# Get the current timestamp
-timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+create_log_entry() {
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    country="iran"
+    city="tehran"
+    population=$((RANDOM * RANDOM))
+    randomshortint=$((1 + RANDOM % 100))
+    men=$(($population / 2))
+    women=$(($population / $randomshortint))
+    hOffset=$(($population * 2))
+    vOffset=100
+    weather="sun1.city=(sun1.city / 100) * 90;"
+    json='{"country":'$country', "city": '$city', "population": '$population', "men": '$men', "women": '$women', "hOffset": '$hOffset', "vOffset": '$vOffset', "weather": '$weather'}'
+    log="myfirsylog : $timestamp hello devops:\n$json"
+    echo -e "$log"
+}
 
-# Generate a random long integer for population
-population=$((RANDOM * RANDOM))
-
-# Generate a random short integer
-randomshortint=$((1 + RANDOM % 100))
-
-# Calculate the number of men and women
-men=$(($population / 2))
-women=$(($population / $randomshortint))
-
-# Calculate hOffset
-hOffset=$(($population * 2))
-
-# Prepare the log message
-log_message="myfirsylog : $timestamp hello devops\n: {\"iran\"\n \"city\": \"tehran\",\n \"population\": \"$population\",\n \"men\": \"$men\",\n \"women\": \"$women\",\n \"hOffset\": \"$hOffset\",\n \"vOffset\": 100,\n \"weather\": \"sun1.city=(sun1.city / 100)*90;\"\n}"
-
-# Send the log message to the local port (e.g., port 8080)
-echo -e $log_message | nc localhost 8080
+while true; do
+    log_record=$(create_log_entry)
+    echo "${log_record}" | nc localhost 8080
+    sleep 3
+done
